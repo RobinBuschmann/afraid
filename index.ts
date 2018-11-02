fcd0b17eba1af0544a69c039b5796efb3fe3eec0
RobinBuschmann <robinbuschmann@hotmail.de>
2018-11-02 09:53:42 +0100

    Removes prototyped code from index.ts

--- index.ts
+++ index.ts
@@ -1,20 +1,6 @@
 ///<reference path="lib/express-extension.d.ts"/>
 
-import * as express from 'express';
-import {body} from './lib/middlewares/middlwares';
-import {f} from './lib/meta/functional/field';
-
 export * from './lib/middlewares/middlwares';
 export * from './lib/meta/functional/field';
 export * from './lib/meta/class/field';
 export * from './lib/meta/class/decorators';
-
-
-const app = express();
-
-app.post('/test', [
-    body(
-        f('test')
-    ),
-], (req, res, next) => {
-});
